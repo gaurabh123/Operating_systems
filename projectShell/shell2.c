b3e925baa0340f136055fee0db744694adc92d84
Gaurav Adhikari <65755032+gaurabh123@users.noreply.github.com>
2024-11-15 15:04:58 -0500

    Update shell2.c

--- projectShell/shell2.c
+++ projectShell/shell2.c
@@ -1,3 +1,4 @@
+// Author: Saharsha Tiwari & Sameer Acharya
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,79 +9,96 @@
 #include <signal.h>
 #include <errno.h>
 
-#define CMD_MAX_LEN 1024
-#define CMD_MAX_ARGS 128
-#define MAX_BG_PROCESSES 100
+#define MAX_COMMAND_LINE_LEN 1024
+#define MAX_COMMAND_LINE_ARGS 128
+#define MAX_BACKGROUND_PROCESSES 100
 
-char shell_prompt[] = "$ ";
-char token_delimiters[] = " \t\r\n";
-int arg_count = 0;
+char prompt[] = "> ";
+char delimiters[] = " \t\r\n";
+int argumentsCounts = 0;
 extern char **environ;
-pid_t bg_processes[MAX_BG_PROCESSES];
-int bg_process_count = 0;
-int is_cmd_running = 0;
-pid_t fg_process = 0;
+pid_t background_processes[MAX_BACKGROUND_PROCESSES];
+int num_background_processes = 0;
+int is_process_running = 0;
+pid_t foreground_process = 0;
 
-void split_input(char input[], char *args[]) {
-    char *token = strtok(input, token_delimiters);
-    while (token != NULL) {
-        char *env_var = strchr(token, '$');
-        if (env_var) {
-            char *env_name = env_var + 1;
-            char *env_val = getenv(env_name);
-            args[arg_count] = env_val;
+void tokenizer(char command_line[], char *arguments[] ) {
+    char *token = strtok(command_line, delimiters);
+    while(token != NULL) {
+        char *dollarSign = strchr(token, '$');
+        if (dollarSign) {
+            char *varName = dollarSign + 1;
+            char *env_value = getenv(varName);
+            arguments[argumentsCounts] = env_value;
         } else {
-            args[arg_count] = token;
+            arguments[argumentsCounts] = token;
+            argumentsCounts++;
+            token = strtok(NULL, delimiters);
         }
-        arg_count++;
-        token = strtok(NULL, token_delimiters);
-    }
-    args[arg_count] = NULL;
-}
-
-void fg_process_handler(int sig) {
-    if (fg_process > 0) {
-        kill(fg_process, SIGTERM);
-        printf("\nForeground process terminated due to timeout (10 seconds).\n");
     }
 }
 
-void execute_command(char *args[]) {
-    is_cmd_running = 1;
-    char cwd[512];
-    if (strcmp(args[0], "cd") == 0) {
-        if (arg_count < 2) {
+
+void foreground_process_handler(int signum) {
+    if (foreground_process > 0) {
+        // Terminate the foreground process
+        kill(foreground_process, SIGTERM);
+        printf("\nTerminated .. Time limit exceeded 10 seconds  .\n");
+    }
+}
+
+void executeCommands(char *arguments[]) {
+    is_process_running = 1;
+    char buff[512];
+    if (strcmp(arguments[0], "cd") == 0) {
+        if (argumentsCounts < 2) {
             if (chdir(getenv("HOME")) != 0) {
                 perror("chdir failed");
             }
-        } else if (arg_count > 2) {
+        } else if (argumentsCounts > 2) {
             fprintf(stderr, "Error: Too many arguments.\n");
-        } else if (chdir(args[1]) != 0) {
+        }else if (chdir(arguments[1]) != 0 ) {
             perror("chdir failed");
+            if (errno == ENOENT) {
+                fprintf(stderr, "Directory '%s' does not exist.\n", arguments[1]);
+            } else if (errno == EACCES) {
+                fprintf(stderr, "Permission denied for directory '%s'.\n", arguments[1]);
+            } else if (errno == ENOTDIR) {
+                fprintf(stderr, "'%s' is not a directory.\n", arguments[1]);
+            } else {
+                fprintf(stderr, "Failed to change directory: %s\n", strerror(errno));
+            }
         }
-    } else if (strcmp(args[0], "pwd") == 0) {
-        getcwd(cwd, sizeof(cwd));
-        printf("%s\n", cwd);
-    } else if (strcmp(args[0], "exit") == 0) {
+    } else if (strcmp(arguments[0],"pwd") == 0) {
+        getcwd(buff, sizeof(buff));
+        printf("%s\n", buff);
+    } else if (strcmp(arguments[0], "exit") == 0) {
         exit(0);
-    } else if (strcmp(args[0], "echo") == 0) {
-        for (int i = 1; i < arg_count; i++) {
-            if (args[i] != NULL) {
-                printf("%s ", args[i]);
+    } else if (strcmp(arguments[0], "echo") == 0) {
+        int i = 1;
+        for (;i < argumentsCounts;i++) {
+            if (arguments[i] != NULL) {
+                printf("%s ", arguments[i]);
             }
         }
         printf("\n");
-    } else if (strcmp(args[0], "env") == 0) {
-        for (char **env = environ; *env != NULL; env++) {
+
+    } else if (strcmp(arguments[0], "env") ==0 ) {
+        char **env = environ;
+        while (*env) {
             printf("%s\n", *env);
+            env++;
         }
-    } else if (strcmp(args[0], "setenv") == 0) {
-        for (int i = 1; i < arg_count; i++) {
-            char *var = strtok(args[i], "=");
-            if (var != NULL) {
-                char *val = strtok(NULL, "=");
-                if (val != NULL) {
-                    if (setenv(var, val, 1) != 0) {
+    } else if (strcmp(arguments[0] , "setenv") ==0 ) {
+        int i = 1;
+        for (;i < argumentsCounts; i++ ) {
+            char *token = strtok(arguments[i], "=");
+            if (token != NULL) {
+                char *var_name = token;
+                token = strtok(NULL, "=");
+                if (token != NULL) {
+                    char *var_value = token;
+                    if (setenv(var_name, var_value, 1) != 0) {
                         perror("setenv failed");
                     }
                 } else {
@@ -89,96 +107,154 @@ void execute_command(char *args[]) {
             } else {
                 printf("Invalid input format.\n");
             }
+
         }
     } else {
-        int run_in_bg = 0;
-        if (arg_count > 0 && strcmp(args[arg_count - 1], "&") == 0) {
-            run_in_bg = 1;
-            args[arg_count - 1] = NULL;
+        int run_in_background = 0;
+        if (argumentsCounts > 0 && strcmp(arguments[argumentsCounts - 1], "&") == 0) {
+            run_in_background = 1;
+            // Remove the "&" from the arguments
+            arguments[argumentsCounts - 1] = NULL;
         }
         pid_t pid = fork();
         if (pid < 0) {
             perror("Fork failed");
             exit(1);
         } else if (pid == 0) {
-            execvp(args[0], args);
+            arguments[argumentsCounts] = NULL;
+            execvp(arguments[0], arguments);
             perror("execvp failed");
+            fprintf(stderr, "An error occurred.\n");
             exit(1);
         } else {
-            if (!run_in_bg) {
-                signal(SIGALRM, fg_process_handler);
+            if (!run_in_background) {
+                signal(SIGALRM, foreground_process_handler);
                 alarm(10);
-                fg_process = pid;
+                foreground_process = pid;
                 int status;
                 waitpid(pid, &status, 0);
                 alarm(0);
-                fg_process = 0;
+                foreground_process = 0;
             } else {
-                if (bg_process_count < MAX_BG_PROCESSES) {
-                    bg_processes[bg_process_count++] = pid;
+                // Add the background process to the list
+                if (num_background_processes < MAX_BACKGROUND_PROCESSES) {
+                    background_processes[num_background_processes] = pid;
+                    num_background_processes++;
                 } else {
-                    fprintf(stderr, "Maximum background process limit reached.\n");
+                    fprintf(stderr, "Maximum number of background processes reached.\n");
                 }
             }
         }
     }
 }
 
-void sigint_handler(int sig) {
-    if (is_cmd_running) {
-        printf("\n(Use Ctrl-C to interrupt)\n");
+void sigint_handler(int signum) {
+    if (is_process_running) {
+        printf("\n\t\t\t(type ctrl-C)\n");
     } else {
         printf("\n");
     }
     fflush(stdout);
 }
 
-void handle_bg_processes(int sig) {
-    for (int i = 0; i < bg_process_count; i++) {
-        pid_t bg_pid = waitpid(bg_processes[i], NULL, WNOHANG);
+
+// Signal handler for background process termination
+void background_processes_handler(int signum) {
+    int i = 0;
+    for (; i < num_background_processes; i++) {
+        pid_t bg_pid = waitpid(background_processes[i], NULL, WNOHANG);
         if (bg_pid > 0) {
-            printf("Background process %d completed.\n", bg_pid);
-            for (int j = i; j < bg_process_count - 1; j++) {
-                bg_processes[j] = bg_processes[j + 1];
+            printf("Background process with PID %d has completed.\n", bg_pid);
+            // Remove the completed process from the list
+            int j = i;
+            for (; j < num_background_processes - 1; j++) {
+                background_processes[j] = background_processes[j + 1];
             }
-            bg_process_count--;
-            i--;
+            num_background_processes--;
+            i--; // Recheck the same index
         }
     }
 }
 
 int main() {
-    char input_line[CMD_MAX_LEN];
-    char cwd[256];
-    char *args[CMD_MAX_ARGS];
+    // Stores the string typed into the command line.
+    char command_line[MAX_COMMAND_LINE_LEN];
+    char cmd_bak[MAX_COMMAND_LINE_LEN];
 
-    signal(SIGCHLD, handle_bg_processes);
+    char buff[256];
+    // Stores the tokenized command line input.
+    char *arguments[MAX_COMMAND_LINE_ARGS];
+
+    signal(SIGCHLD, background_processes_handler);
     signal(SIGINT, sigint_handler);
 
     while (true) {
-        arg_count = 0;
-        if (getcwd(cwd, sizeof(cwd)) == NULL) {
-            perror("Error getting current directory");
-        } else {
-            printf("%s%s", cwd, shell_prompt);
-            fflush(stdout);
-        }
 
-        if (fgets(input_line, CMD_MAX_LEN, stdin) == NULL && ferror(stdin)) {
-            perror("fgets error");
-            exit(1);
-        }
+        do{
+            argumentsCounts = 0;
+            // Print the shell prompt.
+            if (getcwd(buff, sizeof(buff)) == NULL) {
+                perror("Error : Could not read the current directory");
+            }
+            else {
+                printf("%s%s", buff, prompt);
+                fflush(stdout);
+            }
 
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
+
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);
+            }else {
+                tokenizer(command_line, arguments);
+                executeCommands(arguments);
+                memset(*arguments, 0, sizeof(arguments));
+                is_process_running = 0;
+            }
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+
+
+        // If the user input was EOF (ctrl+d), exit the shell.
         if (feof(stdin)) {
             printf("\n");
-            break;
+            fflush(stdout);
+            fflush(stderr);
+            return 0;
         }
+        // TODO:
+        //
 
-        split_input(input_line, args);
-        if (arg_count > 0) {
-            execute_command(args);
-        }
+        // 0. Modify the prompt to print the current working directory
+
+
+        // 1. Tokenize the command line input (split it on whitespace)
+
+
+        // 2. Implement Built-In Commands
+
+
+        // 3. Create a child process which will execute the command line input
+
+
+        // 4. The parent process should wait for the child to complete unless its a background process
+
+
+        // Hints (put these into Google):
+        // man fork
+        // man execvp
+        // man wait
+        // man strtok
+        // man environ
+        // man signals
+
+        // Extra Credit
+        // man dup2
+        // man open
+        // man pipes
     }
-
-    return 0;
+    // This should never be reached.
+    return -1;
 }
